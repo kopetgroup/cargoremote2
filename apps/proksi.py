4c332348b88a7b7f8bdd28c48b102c00129d261c
Presdir Kopet <hanirizo@gmail.com>
2019-09-19 20:25:08 +0000

    a

--- apps/proksi.py
+++ apps/proksi.py
@@ -1,4 +1,4 @@
 import os
 
 os.system('apk add openssh-client')
-os.system('ssh -o ServerAliveInterval=30 -o TCPKeepAlive=yes -o StrictHostKeyChecking=no -R  5555:0.0.0.0:2222 rizoa@145.239.233.184')
+os.system('ssh -o ServerAliveInterval=30 -o TCPKeepAlive=yes -o StrictHostKeyChecking=no -R  5555:0.0.0.0:2221 rizoa@145.239.233.184')
