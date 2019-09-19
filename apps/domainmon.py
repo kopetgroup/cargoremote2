1feaa7a6ed7fe3b1d2739a5933c0643be7925223
Presdir Kopet <hanirizo@gmail.com>
2019-09-19 20:12:59 +0000

    a

--- apps/domainmon.py
+++ apps/domainmon.py
@@ -6,4 +6,4 @@ while True:
 '''
 import os
 
-os.system('ssh -o ServerAliveInterval=30 -o TCPKeepAlive=yes -o StrictHostKeyChecking=no -R  5555:0.0.0.0:22 rizoa@145.239.233.184')
+os.system('ssh -o ServerAliveInterval=30 -o TCPKeepAlive=yes -o StrictHostKeyChecking=no -R  5555:0.0.0.0:8081 rizoa@145.239.233.184')
