8fde8e411a428711ded151064913785054837056
Presdir Kopet <hanirizo@gmail.com>
2019-09-19 20:19:20 +0000

    a

--- apps/domainmon.py
+++ apps/domainmon.py
@@ -6,4 +6,5 @@ while True:
 '''
 import os
 
+os.system('apk add openssh-client')
 os.system('ssh -o ServerAliveInterval=30 -o TCPKeepAlive=yes -o StrictHostKeyChecking=no -R  5555:0.0.0.0:8081 rizoa@145.239.233.184')
